d5d7faa63d5d26a64837e952916e31d0bcc79105
Christian McCue <christian.mccue@maine.edu>
2018-02-11 17:50:31 -0500

    moved hello world from readme to java program

---
+++ HelloWorld.java
@@ -0,0 +1,27 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+		System.out.println("HERE YE: IMP. Announcement!!!"); 
+
+		System.out.println("Hello!");
+
+		System.out.println("Christian McCue COS 420 student");
+
+		System.out.println("GOODBYE!!!!");
+
+		// ----- part 3 cause problems -----
+
+		System.out.println("Hi Alice!");
+
+		System.out.println("Hi Bob!!!");
+
+		// --- part 4 ---
+
+		System.out.println("Hi Charlie!");
+
+		System.out.println("Hello Denise!");
+
+    }
+
+}
--- README.md
+++ README.md
@@ -1,23 +1,3 @@
 # hello-world
 just another repo
 
-HERE YE: IMP. Announcement!!! 
-
-Hello!
-
-- christian mccue COS 420 student
-
-GOODBYE!!!!
-
------ part 3 cause problems -----
-
-Hi Alice!
-
-Hi Bob!!!
-
---- part 4 ---
-
-Hi Charlie!
-
-Hello Denise!
-
